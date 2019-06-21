2ebfddd3df93a6455b55afd462750e0ee06ab734
dazzleshadow <51879695+dazzleshadow@users.noreply.github.com>
2019-06-22 01:59:40 +0800

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,37 +1,37 @@
 CREATE TABLE artist
 (
-	name varchar(10) not null,
-	company varchar(10),
+	name varchar(20) not null,
+	company varchar(20),
 	primary key(name)
 )	ENGINE=INNODB;
 
 CREATE TABLE album
 (
-	name varchar(10) not null,
+	name varchar(20) not null,
 	year int,
-	artist varchar(10),
+	artist varchar(20),
 	primary key(name),
 	foreign key (artist) references artist(name)
 )	ENGINE=INNODB;
 
 CREATE TABLE series
 (
-	name varchar(10) not null,
-	type varchar(10),
+	name varchar(20) not null,
+	type varchar(20),
 	primary key(name)
 )	ENGINE=INNODB;
 
 CREATE TABLE song
 (
 	ID int AUTO_INCREMENT PRIMARY KEY,
-	name varchar(10) not null,
+	name varchar(100) not null,
 	link varchar(11) not null,
-	artist varchar(10),
-	album varchar(10),
-	series varchar(10),
+	artist varchar(20),
+	album varchar(20),
+	series varchar(20),
 	time int not null,
 	foreign key (artist) references artist(name),
-	foreign key (album) references album(name),
+	foreign key (album) references album(name),	
 	foreign key (series) references series(name)
 )	ENGINE=INNODB;
 
